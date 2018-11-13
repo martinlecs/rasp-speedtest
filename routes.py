887d7306a91c2c91fc84a5bbbd4eefc123dbebe2
Martin Le <martinle.cse@gmail.com>
2018-11-14 00:42:10 +1100

    fixed threading error

--- routes.py
+++ routes.py
@@ -1,9 +1,13 @@
-from server import app
 from flask import render_template
+import globals
+from server import app
 from server import speed_tester
-
+import sqlite3
 
 @app.route('/')
 def index():
-    data = speed_tester.get_results()
+    with sqlite3.connect(globals.DB_PATH) as conn:
+        c = conn.cursor()
+        c.execute('SELECT * FROM results')
+        data = c.fetchall()
     return render_template('index.html', data=data)
